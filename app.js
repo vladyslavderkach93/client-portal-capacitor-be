dd3e7dc95607300092d2acb526d4f82e8ce33338
Derkach Vladyslav <v.derkach@insart.com>
2024-08-19 22:37:49 +0200

    Updated for Heroku deployment

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
-const port = 3001;
+const port =  process.env.PORT || 3001;
 
 // Middleware
 app.use(cors());
